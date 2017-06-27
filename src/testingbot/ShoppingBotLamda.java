dd839d44f995ed28dfeab220620079555893c207
Thienthai Chotchai <thienthaichotchai@Thienthais-MacBook-Pro.local>
2017-06-28 01:11:54 +0700

    ice commit

---
+++ src/testingbot/ShoppingBotLamda.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testingbot;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+/**
+ *
+ * @author thienthaichotchai
+ */
+public class ShoppingBotLamda implements RequestHandler<Object, Object> {
+
+    @Override
+    public Object handleRequest(Object input,Context context){
+        return null;
+    }
+   
+}
